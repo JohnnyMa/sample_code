7e611e81def674c27062fbb5bf57a0ec4936b79f
johnnyma <johnnyma1988@gmail.com>
2012-09-13 15:11:24 +0800

    refine core.

--- js/core.js
+++ js/core.js
@@ -2,126 +2,32 @@
 //define global variables & utility functions here
 //author: johnny.ma
 
-var sample = {};
-sample.offline = true;
-sample.debug = true;
-sample.constants = {};
-sample.ui = {};
-sample.data = {};
-sample.util = {};
+(function(window, undefined) {
+  var root = this, document = window.document, navigator = window.navigator, location = window.location, sample = {};
+  sample.offline = true;
+  sample.debug = true;
+  sample.constants = {};
+  sample.ui = {};
+  sample.data = {};
+  sample.util = {};
+  sample.services = {};
+
+  sample.constants.HOSTNAME = 'http://127.0.0.1/';
+  //TODO
+  sample.constants.URL_LOGIN = sample.offline ? '../json/login.json' : sample.constants.HOSTNAME + 'login.php';
+
+  window.sample = sample;
+})(window);
 
 //define util functions
 sample.util = (function() {
-  var _setCookie = function(key, value) {
-    document.cookie = ("lang=" + value);
-  };
-
-  var _getCookie = function() {
-    var returnVal = "";
-    var cookieValue = document.cookie;
-    var cookies = cookieValue.split(";");
-    var cookiesValue = new Array();
-    for (var i = 0; i < cookies.length; i++) {
-      var tempArr = cookies[i].split("=");
-      cookiesValue[i] = tempArr;
-    }
-
-    for (var i = 0; i < cookiesValue.length; i++) {
-      if (cookiesValue[i][0] === key) {
-        returnVal = cookiesValue[i][1];
-      }
-    }
-
-    return returnVal;
-  };
-
-  // util function for ajax call
-  // @param {Object} options = {url: string, async: boolean, data: object, success: callback, showOverlay: boolean}
-  var _sendAjax = function(options) {
-    if (options.showOverlay) {
-      pal.ui.loadingOverlayContainer.open();
-    }
-    options.dataType = options.dataType || "JSONP";
-    $.ajax({
-      url : options.url,
-      async : options.async,
-      type : "GET",
-      dataType : options.dataType,
-      data : options.data,
-      success : options.success,
-      complete : function() {
-        if (sample.debug) {
-          console.log('complete.....');
-        }
-        if (options.showOverlay) {
-          pal.ui.loadingOverlayContainer.close();
-        }
-        if (!options.complete) {
-          options.complete
-        }
-      },
-      error : function(res, e, c) {
-        if (options.showOverlay) {
-          pal.ui.loadingOverlayContainer.close();
-          pal.ui.errorOverlayContainer.open();
-        }
-        if (!options.error) {
-          options.error
-        }
-      }
-    });
-  };
-
-  // format number
-  // e.g. 12000 => 1,2000
-  // @param amtStr number
-  var _formatIntNum = function(amtStr) {
-    var isInt = function(num) {
-      return (num % 1 === 0);
-    };
-    var amtStr = (isInt(amtStr)) ? amtStr : Number(amtStr).toFixed(0);
-    amtStr = "" + amtStr;
-    var a, renum = '';
-    var j = 0;
-    var a1 = '', a2 = '', a3 = '';
-    var tes = /^-/;
-    var isCurrency = ( typeof (isCurrency) != 'undefined') ? isCurrency : true;
-
-    a = amtStr.replace(/,/g, "");
-    a = a.replace(/[^-\.,0-9]/g, "");
-    a = a.replace(/(^\s*)|(\s*$)/g, "");
-    if (tes.test(a))
-      a1 = '-';
-    else
-      a1 = '';
-    a = a.replace(/-/g, "");
-    if (a != "0" && a.substr(0, 2) != "0.")
-      a = a.replace(/^0*/g, "");
-    j = a.indexOf('.');
-    if (j < 0)
-      j = a.length;
-    a2 = a.substr(0, j);
-    a3 = a.substr(j);
-    j = 0;
-    for ( i = a2.length; i > 3; i = i - 3) {
-      renum = "," + a2.substr(i - 3, 3) + renum;
-      j++;
-    }
-
-    renum = a1 + a2.substr(0, a2.length - j * 3) + renum + a3;
-
-    return renum;
-  };
-
   var _strToJson = function(str) {
     var json = (new Function("return " + str))();
     return json;
   };
 
   //load a javascript file into current file
-  var _loadScript = function(document, sURL) {
-    console.log(typeof document);
-    console.log(document.body);
+  var _loadScript = function(sURL) {
     var dScript = document.createElement('script');
     dScript.type = 'text/javascript';
     dScript.src = sURL;
@@ -143,10 +49,6 @@ sample.util = (function() {
   };
 
   return {
-    setCookie : _setCookie,
-    getCookie : _getCookie,
-    sendAjax : _sendAjax,
-    formatIntNum : _formatIntNum,
     strToJson : _strToJson,
     loadScript : _loadScript,
     loadScriptString : _loadScriptString
@@ -156,8 +58,21 @@ sample.util = (function() {
 //define common functions for ui rendering
 sample.ui = (function() {
   var _renderHeader = function(options) {
-    //TODO: add code to create header
+    var dElement = document.createElement(options.tagName);
+    dElement.id = 'header';
+    dElement.textContent = options.content;
+    document.body.appendChild(dElement);
+  };
 
+  var _renderContainer = function(options) {
+    var dElement = document.createElement(options.tagName);
+    dElement.id = options.id || '';
+    console.log(options.className);
+    if (undefined !== options.className) {
+      dElement.className = options.className;
+    }
+    dElement.textContent = options.content || '';
+    document.body.appendChild(dElement);
   };
 
   var _renderTable = function(options) {
@@ -170,6 +85,11 @@ sample.ui = (function() {
     //1. add footer content
     //2. add copyright information
     //3. etc.
+    var dElement = document.createElement(options.tagName);
+    dElement.id = 'footer';
+    dElement.className = options.className || "";
+    dElement.textContent = options.content || "";
+    document.body.appendChild(dElement);
   };
 
   //used to render a overlay feature, commonly used when loading page or waiting for ajax response, etc.
@@ -181,7 +101,8 @@ sample.ui = (function() {
     renderHeader : _renderHeader,
     renderTable : _renderTable,
     renderFooter : _renderFooter,
-    renderOverlay : _renderOverlay
+    renderOverlay : _renderOverlay,
+    renderContainer : _renderContainer
   };
 })();
 
